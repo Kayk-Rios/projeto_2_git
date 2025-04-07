e95851ed5335e783e654d965b50e84d51b7e3a8e
Kayk_Darios <manjac688@gmail.com>
2025-04-07 11:52:12 -0300

    feat: injetando componente na pagina principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+import { Cabecalho } from "./components/Cabecalho";
+import { FormularioBusca } from "./components/FormularioBusca";
 
+
+
+interface GitHubUser {
+  login: string;
+  avatar_url: string;
+  bio: string;
+  name: string;
+}
 function App() {
-  
+  const [username, setUsername] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   
 
+  
+  
   return (
     <div className="min-h-screen bg-black text-white flex flex-col items-center pt-10">
-      <h1>Ola mundo</h1>
+      <Cabecalho />
+
+       <FormularioBusca
+        username={username}
+        isLoading={isLoading}
+        onUsernameChange={setUsername}
+        
+      />
+
+
     </div>
   );
 }
