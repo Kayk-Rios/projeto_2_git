cdadb37f5c75de309a9a0975bd1db59371a41a0b
Kayk_Darios <manjac688@gmail.com>
2025-04-07 13:28:20 -0300

    style: formulario buscar

--- src/components/FormularioBusca.tsx
+++ src/components/FormularioBusca.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Search, Loader2 } from 'lucide-react';
+import React from "react";
+import { Search, Loader2 } from "lucide-react";
 
 interface FormularioBuscaProps {
   username: string;
@@ -8,7 +8,12 @@ interface FormularioBuscaProps {
   onSubmit: (e: React.FormEvent) => Promise<void>;
 }
 
-export function FormularioBusca({ username, isLoading, onUsernameChange, onSubmit }: FormularioBuscaProps) {
+export function FormularioBusca({
+  username,
+  isLoading,
+  onUsernameChange,
+  onSubmit,
+}: FormularioBuscaProps) {
   return (
     <form onSubmit={onSubmit} className="w-full max-w-md mb-8">
       <div className="relative">
@@ -17,12 +22,16 @@ export function FormularioBusca({ username, isLoading, onUsernameChange, onSubmi
           value={username}
           onChange={(e) => onUsernameChange(e.target.value)}
           placeholder="Digite um usuário do Github"
-          className="w-full px-3 py-2 rounded-lg bg-white-800 text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-3 py-[20px] rounded-lg bg-white-800 text-black
+           placeholder-gray-400 focus:outline-none focus:ring-2
+            focus:ring-blue-500"
         />
         <button
           type="submit"
           disabled={isLoading}
-          className="absolute right-2 top-1/2 -translate-y-1/2 bg-blue-600 p-2 rounded-lg hover:bg-blue-700 transition-colors disabled:bg-blue-800 disabled:cursor-not-allowed"
+          className="absolute right-[0.2px] top-1/2 -translate-y-1/2 bg-blue-600 p-[21px]
+           rounded-lg hover:bg-blue-700 transition-colors
+            disabled:bg-blue-800 disabled:cursor-not-allowed"
         >
           {isLoading ? (
             <Loader2 size={20} className="animate-spin" />
