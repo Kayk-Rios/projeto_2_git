f55edb60a57358fa1b2c1c5c5b5d9aaf7bdbec75
Kayk_Darios <manjac688@gmail.com>
2025-04-07 12:17:03 -0300

    style: deixando mais proximo do figma campo de busca

--- src/components/FormularioBusca.tsx
+++ src/components/FormularioBusca.tsx
@@ -17,7 +17,7 @@ export function FormularioBusca({ username, isLoading, onUsernameChange, onSubmi
           value={username}
           onChange={(e) => onUsernameChange(e.target.value)}
           placeholder="Digite um usuário do Github"
-          className="w-full px-4 py-2 rounded-lg bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-3 py-2 rounded-lg bg-white-800 text-black placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <button
           type="submit"
