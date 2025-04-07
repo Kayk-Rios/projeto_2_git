4847e90aef6b8049990bc8ca7fcf09ca3358745e
Kayk_Darios <manjac688@gmail.com>
2025-04-07 12:28:32 -0300

    feat/style: utilizando icon github cabeçalho

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -1,10 +1,10 @@
-import { Github } from 'lucide-react';
-
+import logo from '../img/image 1.png'
 export function Cabecalho() {
   return (
     <div className="flex items-center gap-2 mb-8">
-      <Github size={40} />
-      <h1 className="text-3xl font-bold">Perfil GitHub</h1>
+      <img src={logo} alt="Logo"  className='w-[40px] h-[40px]'/>
+
+      <h1 className="text-3xl">Perfil <span className='font-bold text-4xl'>GitHub</span></h1>
     </div>
   );
 }
