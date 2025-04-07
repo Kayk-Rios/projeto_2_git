79c47e5bf0f71b2ad400e8ba79063982d9cbcd99
Kayk_Darios <manjac688@gmail.com>
2025-04-07 11:32:38 -0300

    chore: start padrao do react

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  
+
+  
+
+  return (
+    <div className="min-h-screen bg-black text-white flex flex-col items-center pt-10">
+      <h1>Ola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
