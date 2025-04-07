9e645a6e39b8b5159239b6d64cbe6fccff73cc70
Kayk_Darios <manjac688@gmail.com>
2025-04-07 13:06:11 -0300

    style: card erro, mudando cor de fundo

--- src/components/MensagemErro.tsx
+++ src/components/MensagemErro.tsx
@@ -4,6 +4,6 @@ interface MensagemErroProps {
 
 export function MensagemErro({ message }: MensagemErroProps) {
   return (
-    <div className="text-red-500 mb-4">{message}</div>
+    <div className="text-red-500 mb-4  bg-white p-[35px] border rounded-md" >{message}</div>
   );
 }
